51d18d6eb418e7c80b12218fd31172b869f554a8
Shauryacious <shauryaagrawal2718@gmail.com>
2024-09-24 15:41:53 +0530

    ok

--- codeForces/IITPatna/Contest-10/Qh.cpp
+++ codeForces/IITPatna/Contest-10/Qh.cpp
@@ -1,167 +1,88 @@
-// F1. Guess the K-th Zero (Easy version)
-// D. Pythagorean Triples
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <chrono>
-#include <random>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <unordered_set>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <bitset>
-#include <numeric>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
-// Speed
-#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
-
-// Define Constants
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nline "\n"
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define PI 3.141592653589793238462
-#define set_bits __builtin_popcountll
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-// Typedef
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-
-
-/*---------------------------------------------------------------------------------------------------------------------------*/
-#ifndef ONLINE_JUDGE
-    #define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
-#else
-    #define debug(x)
-#endif
-
-// DEEBUG
-
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
-
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-// void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-/*---------------------------------------------------------------------------------------------------------------------------*/
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); //generates random numbers
-/*---------------------------------------------------------------------------------------------------------------------------*/
-ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
-ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
-void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 10; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
-ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
-ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
-ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return mod_add(arr[0], 0, b);} //for non prime b
-ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
-bool revsort(ll a, ll b) {return a > b;}
-ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
-ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
-void google(int t) {cout << "Case #" << t << ": ";}
-vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
-ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
-ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);} 
-/*---------------------------------------------------------------------------------------------------------------------------*/
-vector<ll> sieve(ll n) {vector<ll> isPrime(n + 1, 1);for (ll i = 2; i * i <= n; i++) { if (isPrime[i] == 1) {for (ll j = i * i; j <= n; j += i) { isPrime[j] = 0;}}}vector<ll> primes;for (ll i = 2; i <= n; i++) {if (isPrime[i]) {primes.push_back(i);}}return primes;}
-/*---------------------------------------------------------------------------------------------------------------------------*/
-
-
-// Macros
-#define rep(i, j) for (int i = 0; i < j; i++)
-#define invec(v, n) for (int i = 0; i < n; i++) cin >> v[i]
-#define sumvec(v) accumulate(v.begin(), v.end(), 0)
-#define sortvec(v) sort(v.begin(), v.end())
-#define revsortvec(v) sort(v.rbegin(), v.rend())
-#define maxvec(v) *max_element(v.begin(), v.end())
-#define minvec(v) *min_element(v.begin(), v.end())
-/*---------------------------------------------------------------------------------------------------------------------------*/
+struct Process{
+    int pId;           
+    int arrivalTime;    
+    int burstTime;      
+    int completionTime; 
+    int turnAroundTime; 
+    int waitingTime;    
+};
 
 
 
-void solve() {
-    ll n; cin >> n;
-    string s; cin>>s;
-    ll c0 = 0, c1 = 0;
-    for(int i=0; i<n; i++){
-        if(s[i] == '0') c0++;
-        else c1++;
+void calculateCompletionTime(vector<Process>& processes){
+    int n = processes.size();
+    processes[0].completionTime = processes[0].arrivalTime + processes[0].burstTime;
+    for(int i = 1; i < n; i++){
+        processes[i].completionTime = processes[i-1].completionTime + processes[i].burstTime;
     }
-    debug(c0);
-    debug(c1);
-    int count = 0;
-    for(int i=1; i<=n; i++){
-        // debug(i);
-        // if(c0 == 0 || c1 == 0){
-        //     if(i%2 == 0){
-        //         count++;
-        //     }
-        //     // continue;
-        // }
-        // if(c0 == 1 || c1 == 1){
-        //     if(i%2 != 0){ // odd
-        //         count++;
-        //     }
-        //     // continue;
-        // }
-        if(c0 != 0 && i >= c0){
-            // cout<<"a"<<nline;
-            int k = i-c0;
-            // debug(k);
-            if(k%2 == 0) count++;
-            // debug(count);
-            continue;
-        }
-        if(c1 != 0 && i >= c1){
-            // cout<<"b"<<nline;
-            int k = i-c1;
-            // debug(k);
-            if(k%2 == 0) count++;
-            // debug(count);
-            continue;
-        }
-    }
-    cout<<count<<nline;
 }
 
-int main() {
-    #ifndef ONLINE_JUDGE
-        freopen("Error.txt", "w", stderr);
-    #endif
-
-    fastio();
-    ll t;
-    cin >> t;
-    while (t--) {
-        solve();
+void calculateTurnAroundTime(vector<Process>& processes){
+    for(auto& process : processes){
+        process.turnAroundTime = process.completionTime - process.arrivalTime;
     }
+}
+
+void calculateWaitingTime(vector<Process>& processes){
+    for(auto& process : processes){
+        process.waitingTime = process.turnAroundTime - process.burstTime;
+        if(process.waitingTime < 0){
+            process.waitingTime = 0;
+        }
+    }
+}
+
+void calculateAverageTimes(vector<Process>& processes){
+    int n = processes.size();
+    calculateCompletionTime(processes);
+    calculateTurnAroundTime(processes);
+    calculateWaitingTime(processes);
+
+    int totalWaitingTime = 0;
+    int totalTurnAroundTime = 0;
+
+    for(auto& process : processes){
+        totalWaitingTime += process.waitingTime;
+        totalTurnAroundTime += process.turnAroundTime;
+    }
+
+    float averageWaitingTime = (float)totalWaitingTime / n;
+    float averageTurnAroundTime = (float)totalTurnAroundTime / n;
+
+    cout << "Average Waiting Time = " << averageWaitingTime << endl;
+    cout << "Average Turn Around Time = " << averageTurnAroundTime << endl;
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<Process> processes(n);
+
+    for(int i = 0; i < n; i++){
+        processes[i].pId = i + 1;
+        cin >> processes[i].arrivalTime >> processes[i].burstTime;
+    }
+
+    sort(processes.begin(), processes.end(), [](const Process& p1, const Process& p2) {
+        return p1.arrivalTime < p2.arrivalTime;
+    });
+
+    calculateAverageTimes(processes);
+
+    cout << endl << "Process details:" << endl;
+    cout << "PID\tAT\tBT\tCT\tTAT\tWT\n";
+    for(const auto& process : processes){
+        cout << process.pId << "\t" << process.arrivalTime << "\t" 
+             << process.burstTime << "\t" << process.completionTime << "\t" 
+             << process.turnAroundTime << "\t" << process.waitingTime << endl;
+    }
+
     return 0;
 }
