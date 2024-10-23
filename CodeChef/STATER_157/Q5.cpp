53869c34bd948395e50ff9080cbb0d1cfb763222
Shauryacious <shauryaagrawal2718@gmail.com>
2024-10-23 19:54:38 +0530

    ok

--- CodeChef/STATER_157/Q5.cpp
+++ CodeChef/STATER_157/Q5.cpp
@@ -111,6 +111,7 @@ vector<ll> sieve(ll n) {vector<ll> isPrime(n + 1, 1);for (ll i = 2; i * i <= n; 
 void solve() {
     ll n; cin >> n;
     vll a(n); invec(a, n);
+    debug(a);
 }
 
 
@@ -120,7 +121,7 @@ int main(){
     #endif
     fastio();
     ll t = 1; 
-    // cin >> t;
+    cin >> t;
     while(t--){
         solve();
     }
