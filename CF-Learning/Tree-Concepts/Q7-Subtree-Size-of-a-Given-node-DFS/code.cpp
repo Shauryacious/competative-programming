ffa3be2ab97d178d3757beddee5d37af9b79c6e8
Shauryacious <shauryaagrawal2718@gmail.com>
2025-07-05 21:46:18 +0530

    chore: ignore macOS .DS_Store files

--- CF-Learning/Tree-Concepts/Q7-Subtree-Size-of-a-Given-node-DFS/code.cpp
+++
@@ -1,9 +0,0 @@
-ll dfs(ll u, ll p, vvll& adj) {
-    ll size = 1; // Include the current node itself
-    for(auto v : adj[u]) {
-        if(v != p) { // Avoid revisiting the parent
-            size += dfs2(v, u, adj);
-        }
-    }
-    return size;
-}
--- CodeChef/STARTER_135/Q2.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int t; cin>>t;
-    while(t--){
-        int x, y; cin>>x>>y;
-        cout<<x%3<<endl;
-    }
-}
